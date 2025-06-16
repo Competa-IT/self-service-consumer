754e513ec8a1a12d521375d41390e812c0202c67
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-16 06:27:04 +0200

    test: Update provisioning api related tests according to changes in the test harness
    Issue: univention/dev/internal/team-nubus#892

--- tests/chart/test_provisioning_api_client.py
+++ tests/chart/test_provisioning_api_client.py
@@ -1,14 +1,21 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
-from univention.testing.helm.client.provisioning_api import ProvisioningApi
+from univention.testing.helm.client.provisioning_api import Auth, Connection
 
 
-class TestProvisioningApiClientConfiguration(ProvisioningApi):
+class TestAuth(Auth):
 
     config_map_name = "release-name-selfservice-consumer-common"
     secret_name = "release-name-selfservice-consumer-provisioning-api"
 
     default_username = "selfservice"
+
+    path_provisioning_api_url = "data.PROVISIONING_API_BASE_URL"
+
+
+class TestConnection(Connection):
+
+    config_map_name = "release-name-selfservice-consumer-common"
 
     path_provisioning_api_url = "data.PROVISIONING_API_BASE_URL"
