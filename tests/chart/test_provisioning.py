c5dda1e9717f1336c94e34fe6261ffec19cef070
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-19 18:23:26 +0200

    feat: Add owner role support to the provisioning API secret
    Issue: univention/dev/internal/team-nubus#892

--- tests/chart/test_provisioning.py
+++ tests/chart/test_provisioning.py
@@ -1,17 +1,55 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
-from univention.testing.helm.client.provisioning_api import Auth, Connection, SecretViaEnv
+import json
+
+from univention.testing.helm.client.provisioning_api import (
+    Auth,
+    AuthPasswordOwner,
+    AuthPasswordSecret,
+    AuthUsername,
+    Connection,
+    SecretViaEnv,
+)
 
 
-class TestAuth(SecretViaEnv, Auth):
-
+class TestAuth(SecretViaEnv, AuthPasswordOwner, Auth):
     config_map_name = "release-name-selfservice-consumer-common"
     secret_name = "release-name-selfservice-consumer-provisioning-api"
 
     default_username = "selfservice"
+    derived_password = "46dc2d43d2cfb31b484b3ded24376b13be696924"
 
-    path_provisioning_api_url = "data.PROVISIONING_API_BASE_URL"
+
+class TestAuthRegistration(AuthPasswordSecret):
+    """Verify that the password value is correctly embedded within the key "registration"."""
+
+    secret_name = "release-name-selfservice-consumer-provisioning-api"
+    path_password = "stringData.registration"
+
+    is_secret_owner = True
+
+    def get_password(self, result):
+        secret = result.get_resource(kind="Secret", name=self.secret_name)
+        registration_json = secret.findone(self.path_password)
+        registration = json.loads(registration_json)
+        return registration["password"]
+
+
+class TestAuthRegistrationUsername(AuthUsername):
+    """Verify that the username is correctly embedded within the key "registration"."""
+
+    secret_name = "release-name-selfservice-consumer-provisioning-api"
+    path_username = "stringData.registration"
+    default_username = "selfservice"
+
+    is_secret_owner = True
+
+    def get_username(self, result):
+        secret = result.get_resource(kind="Secret", name=self.secret_name)
+        registration_json = secret.findone(self.path_username)
+        registration = json.loads(registration_json)
+        return registration["name"]
 
 
 class TestConnection(Connection):
